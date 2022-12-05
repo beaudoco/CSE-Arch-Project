be2c2b055318d24086dd79688240b511412ea625
Beaudoin <cpb5867@psu.edu>
2022-12-05 15:38:25 -0500

    saving figure

--- param_shift.py
+++ param_shift.py
@@ -37,43 +37,27 @@ class QFCModel(QuantumModule):
         x = F.avg_pool2d(x, 6).view(bsz, 16)
 
         if use_qiskit:
-            # print("In FWD")
             x = self.qiskit_processor.process_parameterized(
                 self.q_device, self.encoder, self.q_layer, self.measure, x)
-            # print("Done")
         else:
-            # print("Step 1: \n")
             self.encoder(self.q_device, x)
-            # print("Step 2: \n")
             self.q_layer(self.q_device)
-            # print("Step 3: \n")
             x = self.measure(self.q_device)
-            # print("Step 4: \n")
 
         x = x.reshape(bsz, 4)
-        # print("Step 5: \n")
-
         return x
 
 def grad_calc(param):
     with torch.no_grad():
         param[0].copy_(param[0] + np.pi * 0.5)
-    # print("hello \n")
     out1 = model(param[2], param[3])
-    # print("hello 2\n")
     with torch.no_grad():
         param[0].copy_(param[0] - np.pi)
-    # print("hello \n")
     out2 = model(param[2], param[3])
     with torch.no_grad():
         param[0].copy_(param[0] + np.pi * 0.5)
     grad = 0.5 * (out1 - out2)
-    # file = open("gradients/grad-{0}.txt".format(param[1]), 'w')
-    # file.write(grad)
-    # file.close()
     return grad
-    # np.save("gradients/grad-{0}.npy".format(param[1]), asarray(flatten))
-    # grad_list.append(grad)
 
 def handler(result):
     print(result)
@@ -85,45 +69,24 @@ def shift_and_run(model, inputs, use_qiskit=False):
         param_list.append((param, count, inputs, use_qiskit))
         count += 1
     grad_list = []
-    # procs = []
-    # for param in param_list:
-    #     proc = mp.Process(target=grad_calc, args=(param, use_qiskit))
-    #     procs.append(proc)
-    #     proc.start()
-       
-    # for proc in procs:
-    #     proc.join()
-
-    # for param in param_list:
-    #     grad_list.append(grad_calc(param))
 
     results = Parallel(n_jobs=5)(delayed(grad_calc)(param) for param in param_list)
 
     for res in results:
         grad_list.append(res)
 
-    # if __name__ == '__main__' and use_qiskit:
-    #     print(use_qiskit)
-    #     # pool=mp.Pool(processes=len(param_list))
-    #     with mp.Pool(processes=len(param_list)) as pool:
-    #         proclist = [ pool.apply_async(grad_calc, args, callback=handler) for args in param_list ]
-    #     pool.close()
-    #     for res in proclist:
-    #         print(res)
-    #         grad_list.append(res)
-
     return model(inputs, use_qiskit), grad_list
 
 use_cuda = torch.cuda.is_available()
-# device = torch.device("cuda" if use_cuda else "cpu")
-device = torch.device("cpu")
+device = torch.device("cuda" if use_cuda else "cpu")
+# device = torch.device("cpu")
 model = QFCModel().to(device)
 processor_real_qc = QiskitProcessor(use_real_qc=True, backend_name='ibmq_manila')
 model.set_qiskit_processor(processor_real_qc)
 
 # model.share_memory()
 
-n_epochs = 15
+n_epochs = 2
 optimizer = optim.Adam(model.parameters(), lr=5e-3, weight_decay=1e-4)
 scheduler = CosineAnnealingLR(optimizer, T_max=n_epochs)
 
@@ -219,3 +182,29 @@ for epoch in range(1, n_epochs + 1):
 
 # test
 valid_test(dataflow, 'test', model, device, qiskit=False)
+
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+
+grads_bp = np.array(grads_bp)
+grads_ps = np.array(grads_ps)
+
+n_steps = grads_bp.shape[0]
+n_params = grads_bp.shape[1]
+
+fig, ax_list = plt.subplots(n_params, 1, sharex=True, figsize=(15, 2 * n_params))
+
+for i, ax in enumerate(ax_list):
+  ax.plot(grads_bp[:, i], c="#1f77b4", label="back propagation")
+  ax.scatter(range(n_steps), grads_ps[:, i], c="#ff7f0e", marker="^", label="parameters shift")
+  ax.set_ylabel("grad of param{0}".format(i))
+  ax.set_xlabel("Step")
+  ax.legend()
+  ax.axhline(color='black', lw=0.5)
+
+plt.tight_layout()
+plt.savefig('foo.png')
+plt.savefig('foo.pdf')
+plt.show()
